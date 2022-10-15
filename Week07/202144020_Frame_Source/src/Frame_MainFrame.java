41c6d2177f3fde9f2276e9259c41170108f4d109
아기코쟁이 <90755590+haansohee@users.noreply.github.com>
2022-10-15 16:24:47 +0900

    Week07_Java_Dialog

---
+++ Week07/202144020_Frame_Source/src/Frame_MainFrame.java
@@ -0,0 +1,126 @@
+/**
+ * [Frame 호출]Button : 정보 Frame(frmInformation) 호출
+ *
+ * @author hansohee
+ */
+public class Frame_MainFrame extends javax.swing.JFrame {
+
+    public Frame_MainFrame() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        frmInformation = new javax.swing.JFrame();
+        lblInformation = new javax.swing.JLabel();
+        lblTitle = new javax.swing.JLabel();
+        btnCall = new javax.swing.JButton();
+
+        frmInformation.setTitle("정보");
+
+        lblInformation.setText("이준혁 바보멍충이!!!");
+
+        javax.swing.GroupLayout frmInformationLayout = new javax.swing.GroupLayout(frmInformation.getContentPane());
+        frmInformation.getContentPane().setLayout(frmInformationLayout);
+        frmInformationLayout.setHorizontalGroup(
+            frmInformationLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(frmInformationLayout.createSequentialGroup()
+                .addGap(164, 164, 164)
+                .addComponent(lblInformation)
+                .addContainerGap(135, Short.MAX_VALUE))
+        );
+        frmInformationLayout.setVerticalGroup(
+            frmInformationLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(frmInformationLayout.createSequentialGroup()
+                .addGap(107, 107, 107)
+                .addComponent(lblInformation)
+                .addContainerGap(176, Short.MAX_VALUE))
+        );
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        lblTitle.setText("Frame 호출 예제");
+
+        btnCall.setText("Frame 호출");
+        btnCall.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCallActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(119, 119, 119)
+                .addComponent(lblTitle)
+                .addContainerGap(193, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnCall)
+                .addGap(61, 61, 61))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(15, 15, 15)
+                .addComponent(lblTitle)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 184, Short.MAX_VALUE)
+                .addComponent(btnCall)
+                .addGap(61, 61, 61))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnCallActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCallActionPerformed
+        frmInformation.setLocation(300, 300);
+        frmInformation.setSize(300, 300);
+        frmInformation.setVisible(true);
+    }//GEN-LAST:event_btnCallActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Frame_MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Frame_MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Frame_MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Frame_MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Frame_MainFrame().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnCall;
+    private javax.swing.JFrame frmInformation;
+    private javax.swing.JLabel lblInformation;
+    private javax.swing.JLabel lblTitle;
+    // End of variables declaration//GEN-END:variables
+}
